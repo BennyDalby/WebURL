790d5cfc487a5988a314e731e7406397708abada
CLPricingTeam <clpricingteam@tilaks-mbp.in-101.lan.philips.com>
2015-01-28 11:15:08 +0530

    Initial Commit

---
+++ WebUrl/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WebUrl
+//
+//  Created by CLPricingTeam on 1/28/15.
+//  Copyright (c) 2015 Philips. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
